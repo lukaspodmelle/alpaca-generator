fd4583cbb055d275f63c0b12fe5f2da925cc5d9b
Lukas Podmelle <podmellelukas@gmail.com>
2023-08-29 16:46:47 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,423 @@
+// Init buttons
+const hairButton    =   document.getElementById('hair-button')
+const earsButton    =   document.getElementById('ears-button')
+const eyesButton    =   document.getElementById('eyes-button')
+const mouthButton   =   document.getElementById('mouth-button')
+const neckButton    =   document.getElementById('neck-button')
+const legButton     =   document.getElementById('leg-button')
+const accButton     =   document.getElementById('accessories-button')
+const bgButton      =   document.getElementById('background-button')
+
+const accButtons    =   document.getElementById('accessories__buttons')
+const styleButtons  =   document.getElementById('style__buttons')
+
+const randButton    =   document.getElementById('random')
+const dwnButton     =   document.getElementById('download')
+
+// Init image parts
+const bgPart      =   document.getElementById('image')
+const hairPart    =   document.getElementById('hair-part')
+const earsPart    =   document.getElementById('ears-part')
+const eyesPart    =   document.getElementById('eyes-part')
+const mouthPart   =   document.getElementById('mouth-part')
+const neckPart    =   document.getElementById('neck-part')
+const legPart     =   document.getElementById('leg-part')
+const accPart     =   document.getElementById('acc-part')
+
+// Init titles
+const titleStyle  =   document.getElementById('controls__title-style')
+
+// Init image parts list
+const hair = [{
+    text: "Default",
+    src: "./img/hair/default.png"
+},
+{
+    text: "Bang",
+    src: "./img/hair/bang.png"
+},
+{
+    text: "Curls",
+    src: "./img/hair/curls.png"
+},
+{
+    text: "Elegant",
+    src: "./img/hair/elegant.png"
+},
+{
+    text: "Fancy",
+    src: "./img/hair/fancy.png"
+},
+{
+    text: "Quiff",
+    src: "./img/hair/quiff.png"
+},
+{
+    text: "Short",
+    src: "./img/hair/short.png"
+}]
+
+const eyes = [{
+    text: "Default",
+    src: "./img/eyes/default.png"
+},
+{
+    text: "Angry",
+    src: "./img/eyes/angry.png"
+},
+{
+    text: "Naughty",
+    src: "./img/eyes/naughty.png"
+},
+{
+    text: "Panda",
+    src: "./img/eyes/panda.png"
+},
+{
+    text: "Smart",
+    src: "./img/eyes/smart.png"
+},
+{
+    text: "Star",
+    src: "./img/eyes/star.png"
+}]
+
+const ears = [{
+    text: "Default",
+    src: "./img/ears/default.png"
+},
+{
+    text: "Tilt Backward",
+    src: "./img/ears/tilt-backward.png"
+},
+{
+    text: "Tilt Forward",
+    src: "./img/ears/tilt-forward.png"
+}]
+
+const leg = [{
+    text: "Default",
+    src: "./img/leg/default.png"
+},
+{
+    text: "Bubble Tea",
+    src: "./img/leg/bubble-tea.png"
+},
+{
+    text: "Cookie",
+    src: "./img/leg/cookie.png"
+},
+{
+    text: "Game Console",
+    src: "./img/leg/game-console.png"
+},
+{
+    text: "Tilt Backward",
+    src: "./img/leg/tilt-backward.png"
+},
+{
+    text: "Tilt Forward",
+    src: "./img/leg/tilt-forward.png"
+}]
+
+const mouth = [{
+    text: "Default",
+    src: "./img/mouth/default.png"
+},
+{
+    text: "Astonished",
+    src: "./img/mouth/astonished.png"
+},
+{
+    text: "Eating",
+    src: "./img/mouth/eating.png"
+},
+{
+    text: "Laughing",
+    src: "./img/mouth/laugh.png"
+},
+{
+    text: "Tongue",
+    src: "./img/mouth/tongue.png"
+}]
+
+const neck = [{
+    text: "Default",
+    src: "./img/neck/default.png"
+},
+{
+    text: "Thick",
+    src: "./img/neck/thick.png"
+},
+{
+    text: "Bend Backward",
+    src: "./img/neck/bend-backward.png"
+},
+{
+    text: "Bend Forward",
+    src: "./img/neck/bend-forward.png"
+}]
+
+const accessories = [{
+    text: "None",
+    src: "./img/accessories/empty.png"
+},
+{
+    text: "Earings",
+    src: "./img/accessories/earings.png"
+},
+{
+    text: "Flower",
+    src: "./img/accessories/flower.png"
+},
+{
+    text: "Glasses",
+    src: "./img/accessories/glasses.png"
+},
+{
+    text: "Headphones",
+    src: "./img/accessories/headphone.png"
+}]
+
+const background = [{
+    name: "Blue 50",
+    color: "#5596f6"
+},
+{
+    name: "Blue 60",
+    color: "#2a7bf4"
+},
+{
+    name: "Blue 70",
+    color: "#2a5ca7"
+},
+{
+    name: "Dark Blue 30",
+    color: "#7a8faf"
+},
+{
+    name: "Dark Blue 50",
+    color: "#002966"
+},
+{
+    name: "Dark Blue 70",
+    color: "#00193d"
+},
+{
+    name: "Green 50",
+    color: "#9fd966"
+},
+{
+    name: "Green 60",
+    color: "#74bd29"
+},
+{
+    name: "Green 70",
+    color: "#6a9440"
+},
+{
+    name: "Yellow 50",
+    color: "#fcd33f"
+},
+{
+    name: "Yellow 60",
+    color: "#e6b60d"
+},
+{
+    name: "Yellow 70",
+    color: "#cf8e10"
+},
+{
+    name: "Red 50",
+    color: "#e85e23"
+},
+{
+    name: "Red 60",
+    color: "#cb4c15"
+},
+{
+    name: "Red 70",
+    color: "#9f370a"
+},
+{
+    name: "Grey 40",
+    color: "#cccccc"
+},
+{
+    name: "Grey 70",
+    color: "#666666"
+},
+{
+    name: "Grey 80",
+    color: "#333333"
+},
+]
+
+// Render choices
+function renderButtons(imagePart) {
+    const buttons = []
+
+    for (let i = 0; i < imagePart.length; i++) {
+
+        if (imagePart != background) {
+            const imagePartButton = `<button onclick="updateImage('${imagePart[i].src}')" class="button button__controls">${imagePart[i].text}</button>`
+            buttons.push(imagePartButton)
+        } else {
+            const backgroundButton = `<button onclick="updateBg('${imagePart[i].color}')" class="button__color" name="${imagePart[i].name}"><span class="button__color_swatch" style="background:${imagePart[i].color}"></span></button>`
+            buttons.push(backgroundButton)
+        }
+        
+    }
+    // Remove previous
+    styleButtons.innerHTML = ''
+
+    // Add new
+    buttons.forEach(imagePartButton => {
+        styleButtons.innerHTML += imagePartButton
+    })
+
+    // Active button states
+    styleButtons.querySelectorAll('button').forEach(button => {
+        button.addEventListener('click', () => {
+            removeActiveStyle()
+            button.classList.add('active')
+    })
+    })
+    styleButtons.querySelectorAll('.button__color').forEach(button => {
+        button.addEventListener('click', () => {
+            removeActiveColor()
+            button.classList.add('button__color_active')
+        })
+    })
+}
+
+// Update image parts
+function updateImage(src) {
+    if (src.includes('/hair/')) {
+        hairPart.src = src
+    }
+    if (src.includes('/ears/')) {
+        earsPart.src = src
+    }
+    if (src.includes('/eyes/')) {
+        eyesPart.src = src
+    }
+    if (src.includes('/mouth/')) {
+        mouthPart.src = src
+    }
+    if (src.includes('/neck/')) {
+        neckPart.src = src
+    }
+    if (src.includes('/leg/')) {
+        legPart.src = src
+    }
+    if (src.includes('/accessories/')) {
+        accPart.src = src
+    }
+}
+// Update background
+function updateBg(color) {
+    bgPart.style.backgroundColor = color
+    bgPart.style.transition = ".2s ease-in"
+}
+
+// Remove active class from buttons
+function removeActive() {
+    accButtons.querySelectorAll('button').forEach(button => {
+        button.classList.remove('active')
+        styleButtons.classList.remove('active__colors')
+    })
+}
+function removeActiveStyle() {
+    styleButtons.querySelectorAll('button').forEach(button => {
+        button.classList.remove('active')
+    })
+}
+function removeActiveColor() {
+    styleButtons.querySelectorAll('.button__color').forEach(button => {
+        button.classList.remove('button__color_active')
+    })
+}
+
+// Active button states
+hairButton.addEventListener('click', () => {
+    renderButtons(hair)
+    removeActive()
+    hairButton.classList.add('active')
+    titleStyle.innerHTML = 'Choose Hair:'
+})
+earsButton.addEventListener('click', () => {
+    renderButtons(ears)
+    removeActive()
+    earsButton.classList.add('active')
+    titleStyle.innerHTML = 'Choose Ears:'
+})
+eyesButton.addEventListener('click', () => {
+    renderButtons(eyes)
+    removeActive()
+    eyesButton.classList.add('active')
+    titleStyle.innerHTML = 'Choose Eyes:'
+})
+mouthButton.addEventListener('click', () => {
+    renderButtons(mouth)
+    removeActive()
+    mouthButton.classList.add('active')
+    titleStyle.innerHTML = 'Choose Mouth:'
+})
+neckButton.addEventListener('click', () => {
+    renderButtons(neck)
+    removeActive()
+    neckButton.classList.add('active')
+    titleStyle.innerHTML = 'Choose Neck:'
+})
+legButton.addEventListener('click', () => {
+    renderButtons(leg)
+    removeActive()
+    legButton.classList.add('active')
+    titleStyle.innerHTML = 'Choose Leg:'
+})
+accButton.addEventListener('click', () => {
+    renderButtons(accessories)
+    removeActive()
+    accButton.classList.add('active')
+    titleStyle.innerHTML = 'Choose Accessory:'
+})
+bgButton.addEventListener('click', () => {
+    renderButtons(background)
+    removeActive()
+    bgButton.classList.add('active')
+    styleButtons.classList.add('active__colors')
+    titleStyle.innerHTML = 'Choose Background:'
+})
+
+// Randomize
+function randomize() {
+    const imageParts = [hair, eyes, ears, leg, mouth, neck, accessories]
+    imageParts.forEach(part => {
+        updateImage(part[Math.floor(Math.random() * part.length)].src)
+    })
+    updateBg(background[Math.floor(Math.random() * background.length)].color)
+}
+randButton.addEventListener('click', () => {
+    removeActiveStyle()
+    removeActiveColor()
+    randomize()
+})
+
+// Download
+dwnButton.addEventListener('click', () => {
+    html2canvas(bgPart, {scale: 1}).then(function(canvas) {
+        const fileName = "your-alpaca";
+        const link = document.createElement("a");
+        link.download = fileName + ".png";
+
+        canvas.toBlob(function(blob) {
+            link.href = URL.createObjectURL(blob);
+            link.click();
+        })
+    });
+})
+
+// Init app
+renderButtons(hair)
+hairButton.classList.add('active')
---
+++ prev.js
@@ -0,0 +1,379 @@
+const hairEl = document.querySelector('.hair-el')
+const earEl = document.querySelector('.ears-el')
+const eyeEl = document.querySelector('.eyes-el')
+const mouthEl = document.querySelector('.mouth-el')
+const neckEl = document.querySelector('.neck-el')
+const legEl = document.querySelector('.leg-el')
+const accessoriesEl = document.querySelector('.accessories-el')
+const backgroundEl = document.querySelector('.background-el')
+
+const hairImg = document.querySelector('.hair-img')
+const earsImg = document.querySelector('.ears-img')
+const eyesImg = document.querySelector('.eyes-img')
+const mouthImg = document.querySelector('.mouth-img')
+const neckImg = document.querySelector('.neck-img')
+const legImg = document.querySelector('.leg-img')
+const accessoriesImg = document.querySelector('.accessories-img')
+const random = document.getElementById('random')
+const download = document.getElementById('download')
+const image = document.getElementById('image')
+
+const accessoriesBtn = document.querySelector('.accessories-btns')
+const styleBtns = document.querySelector('.style-btns')
+
+const hair = [{
+        text: "Default",
+        src: "./alpaca/hair/default.png"
+    },
+    {
+        text: "Bang",
+        src: "./alpaca/hair/bang.png"
+    },
+    {
+        text: "Curls",
+        src: "./alpaca/hair/curls.png"
+    },
+    {
+        text: "Elegant",
+        src: "./alpaca/hair/elegant.png"
+    },
+    {
+        text: "Fancy",
+        src: "./alpaca/hair/fancy.png"
+    },
+    {
+        text: "Quiff",
+        src: "./alpaca/hair/quiff.png"
+    },
+    {
+        text: "Short",
+        src: "./alpaca/hair/short.png"
+    }
+]
+const ears = [{
+        text: "Default",
+        src: "./alpaca/ears/default.png",
+        element: 'earEl'
+    },
+    {
+        text: "Tilt-backward",
+        src: "./alpaca/ears/tilt-backward.png",
+        element: 'earEl'
+    },
+    {
+        text: "Tilt-forward",
+        src: "./alpaca/ears/tilt-forward.png",
+        element: 'earEl'
+    }
+]
+const accessories = [{
+        text: "Headphone",
+        src: "./alpaca/accessories/headphone.png"
+    },
+    {
+        text: "Earings",
+        src: "./alpaca/accessories/earings.png"
+    },
+    {
+        text: "Flower",
+        src: "./alpaca/accessories/flower.png"
+    },
+    {
+        text: "Glasses",
+        src: "./alpaca/accessories/glasses.png"
+    }
+]
+const eyes = [{
+        text: "Default",
+        src: "./alpaca/eyes/default.png"
+    },
+    {
+        text: "Angry",
+        src: "./alpaca/eyes/angry.png"
+    },
+    {
+        text: "Naughty",
+        src: "./alpaca/eyes/naughty.png"
+    },
+    {
+        text: "Panda",
+        src: "./alpaca/eyes/panda.png"
+    },
+    {
+        text: "Smart",
+        src: "./alpaca/eyes/smart.png"
+    },
+    {
+        text: "Star",
+        src: "./alpaca/eyes/star.png"
+    }
+]
+const mouth = [{
+        text: "Default",
+        src: "./alpaca/mouth/default.png"
+    },
+    {
+        text: "Astonished",
+        src: "./alpaca/mouth/astonished.png"
+    },
+    {
+        text: "Eating",
+        src: "./alpaca/mouth/eating.png"
+    },
+    {
+        text: "Laugh",
+        src: "./alpaca/mouth/laugh.png"
+    },
+    {
+        text: "Tongue",
+        src: "./alpaca/mouth/tongue.png"
+    }
+]
+const neck = [{
+        text: "Default",
+        src: "./alpaca/neck/default.png"
+    },
+    {
+        text: "Bend-backward",
+        src: "./alpaca/neck/bend-backward.png"
+    },
+    {
+        text: "Bend-forward",
+        src: "./alpaca/neck/bend-forward.png"
+    },
+    {
+        text: "Thick",
+        src: "./alpaca/neck/thick.png"
+    },
+]
+const leg = [{
+        text: "Default",
+        src: "./alpaca/leg/default.png"
+    },
+    {
+        text: "Bubble-tea",
+        src: "./alpaca/leg/bubble-tea.png"
+    },
+    {
+        text: "Cookie",
+        src: "./alpaca/leg/cookie.png"
+    },
+    {
+        text: "Game-console",
+        src: "./alpaca/leg/game-console.png"
+    },
+    {
+        text: "Tilt-backward",
+        src: "./alpaca/leg/tilt-backward.png"
+    },
+    {
+        text: "Tilt-forward",
+        src: "./alpaca/leg/tilt-forward.png"
+    },
+]
+const background = [{
+        text: "Blue 50",
+        src: './alpaca/backgrounds/blue50.png'
+    },
+    {
+        text: "Blue 70",
+        src: './alpaca/backgrounds/blue60.png'
+    },
+    {
+        text: "Blue 70",
+        src: './alpaca/backgrounds/blue70.png'
+    },
+    {
+        text: "Dark-blue 30",
+        src: './alpaca/backgrounds/darkblue30.png'
+    },
+    {
+        text: "Dark-blue 50",
+        src: './alpaca/backgrounds/darkblue50.png'
+    },
+    {
+        text: "Dark-blue 70",
+        src: './alpaca/backgrounds/darkblue70.png'
+    },
+    {
+        text: "Green 50",
+        src: './alpaca/backgrounds/green50.png'
+    },
+    {
+        text: "Green 60",
+        src: './alpaca/backgrounds/green60.png'
+    },
+    {
+        text: "Green 70",
+        src: './alpaca/backgrounds/green70.png'
+    },
+    {
+        text: "Grey 40",
+        src: './alpaca/backgrounds/grey40.png'
+    },
+    {
+        text: "Grey 70",
+        src: './alpaca/backgrounds/grey70.png'
+    },
+    {
+        text: "Grey 80",
+        src: './alpaca/backgrounds/grey80.png'
+    },
+    {
+        text: "Red 50",
+        src: './alpaca/backgrounds/red50.png'
+    },
+    {
+        text: "Red 60",
+        src: './alpaca/backgrounds/red60.png'
+    },
+    {
+        text: "Red 70",
+        src: './alpaca/backgrounds/red70.png'
+    },
+    {
+        text: "Yellow 50",
+        src: './alpaca/backgrounds/yellow50.png'
+    },
+    {
+        text: "Yellow 60",
+        src: './alpaca/backgrounds/yellow60.png'
+    },
+    {
+        text: "Yellow 70",
+        src: './alpaca/backgrounds/yellow70.png'
+    }
+]
+
+function generateButtons(accessory) {
+    const buttons = []
+    const len = accessory.length
+
+    for (let i = 0; i < len; i++) {
+        if (accessory != background) {
+            const btn = `
+            <button onclick="updateImage('${accessory[i].src}')">${accessory[i].text}</button>
+        `
+            buttons.push(btn)
+        } else {
+            const btn = `
+            <button onclick="updateBg('${accessory[i].src}')">${accessory[i].text}</button>
+        `
+            buttons.push(btn)
+        }
+    }
+    // remove existing buttons
+    styleBtns.innerHTML = ''
+
+    // add new buttons
+    buttons.forEach(btn => {
+        styleBtns.innerHTML += btn
+    })
+}
+
+function updateImage(imgSrc) {
+    if (imgSrc.includes("hair")) {
+        hairImg.src = imgSrc
+    }
+    if (imgSrc.includes("ear") && imgSrc.includes('earings') === false) {
+        earsImg.src = imgSrc
+    }
+    if (imgSrc.includes("eye")) {
+        eyesImg.src = imgSrc
+    }
+    if (imgSrc.includes("mouth")) {
+        mouthImg.src = imgSrc
+    }
+    if (imgSrc.includes("accessories")) {
+        accessoriesImg.src = imgSrc
+    }
+    if (imgSrc.includes("neck")) {
+        neckImg.src = imgSrc
+    }
+    if (imgSrc.includes("leg")) {
+        legImg.src = imgSrc
+    }
+}
+
+function updateBg(bgSrc) {
+    const img = document.querySelector('.img')
+    img.style.backgroundImage = `url(${bgSrc})`
+}
+
+function removeActive() {
+    accessoriesBtn.querySelectorAll('button').forEach(btn => {
+        btn.classList.remove('active')
+    })
+}
+
+function randomAccessories() {
+    updateImage(hair[Math.floor(Math.random() * hair.length)].src)
+    updateImage(ears[Math.floor(Math.random() * ears.length)].src)
+    updateImage(accessories[Math.floor(Math.random() * accessories.length)].src)
+    updateImage(eyes[Math.floor(Math.random() * eyes.length)].src)
+    updateImage(mouth[Math.floor(Math.random() * mouth.length)].src)
+    updateImage(neck[Math.floor(Math.random() * neck.length)].src)
+    updateImage(leg[Math.floor(Math.random() * leg.length)].src)
+    updateBg(background[Math.floor(Math.random() * background.length)].src)
+}
+
+download.addEventListener('click', () => {
+    html2canvas(image).then(function(canvas) {
+        const fileName = "alpaca";
+        const link = document.createElement("a");
+        link.download = fileName + ".png";
+
+        canvas.toBlob(function(blob) {
+            link.href = URL.createObjectURL(blob);
+            link.click();
+        })
+    });
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+    generateButtons(hair)
+})
+
+hairEl.addEventListener('click', () => {
+    removeActive();
+    hairEl.classList.add('active')
+    generateButtons(hair)
+})
+earEl.addEventListener('click', () => {
+    removeActive();
+    earEl.classList.add('active')
+    generateButtons(ears)
+})
+accessoriesEl.addEventListener('click', () => {
+    removeActive();
+    accessoriesEl.classList.add('active')
+    generateButtons(accessories)
+})
+eyeEl.addEventListener('click', () => {
+    removeActive();
+    eyeEl.classList.add('active')
+    generateButtons(eyes)
+})
+mouthEl.addEventListener('click', () => {
+    removeActive();
+    mouthEl.classList.add('active')
+    generateButtons(mouth)
+})
+neckEl.addEventListener('click', () => {
+    removeActive();
+    neckEl.classList.add('active')
+    generateButtons(neck)
+})
+legEl.addEventListener('click', () => {
+    removeActive();
+    legEl.classList.add('active')
+    generateButtons(leg)
+})
+backgroundEl.addEventListener('click', () => {
+    removeActive();
+    backgroundEl.classList.add('active')
+    generateButtons(background)
+})
+random.addEventListener('click', () => {
+    randomAccessories()
+})
